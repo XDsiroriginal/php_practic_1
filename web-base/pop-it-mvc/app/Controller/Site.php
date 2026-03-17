5c62dcc134f0054a2c16dc54e4ca24c416d2632e
XDsiroriginal <anna.sawhenko1984@gmail.com>
2026-03-18 01:48:11 +0700

    composer json file create, and project progress

--- web-base/pop-it-mvc/app/Controller/Site.php
+++ web-base/pop-it-mvc/app/Controller/Site.php
@@ -1,13 +1,16 @@
 <?php
 namespace Controller;
+use Src\View;
 class Site
 {
-    public function index(): void
+    public function index(): string
     {
-        echo 'working index';
+        $view = new View();
+        return $view->render('site.hello', ['message' => 'index
+working']);
     }
-    public function hello(): void
-    {
-        echo 'working hello';
+    public function hello(): string
+    {return new View('site.hello', ['message' => 'hello
+working']);
     }
 }
